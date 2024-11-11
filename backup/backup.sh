feb8bcc8b9c17d882f42e085070cf4c4ed8ac706
ivan <statkevich.ivan999@gmail.com>
2024-11-11 08:12:05 -0500

    finished working on backup script

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-SOURCE_FILE_PATH=''
-DESTINATION_DIRECTORY_PATH=''
+source_file_path=""
+destination_directory=""
+year=$(date +"%Y")
+month=$(date +"%m")
+day=$(date +"%d")
+time=$(date +"%H%M%S")
+file_name=""
+new_file_name=""
 
-while getopts "s:d" opt; do
+while getopts "s:d:" opt; do
   case $opt in
     s)
-      SOURCE_FILE_PATH=$OPTARG
+      source_file_path=$(dirname "$OPTARG")
+      file_name=$(basename "$OPTARG")
       ;;
     d)
-      DESTINATION_DIRECTORY_PATH=$OPTARG
+      destination_directory=$OPTARG
       ;;
   esac
 done
+
+if [[ -z "$source_file_path" ]]; then
+  echo "Source file is required"
+  exit 1
+fi
+
+if [[ -z "$destination_directory" ]]; then
+  echo "Destination folder is required"
+  exit 1
+fi
+
+new_file_name="${year}${month}${day}${time}_${file_name}"
+
+cp "${source_file_path}/${file_name}" "${destination_directory}/${new_file_name}"
