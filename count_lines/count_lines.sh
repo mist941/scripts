477e0e7798cc2876d072185e45fb950fd3a078c1
ivan <statkevich.ivan999@gmail.com>
2024-11-13 04:01:54 -0500

    finished line counter

--- count_lines/count_lines.sh
+++ count_lines/count_lines.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+file_path=""
+file_name=""
+
+while getopts "f:" opt; do
+  case $opt in
+    f)
+      file_path=$(dirname "$OPTARG")
+      file_name=$(basename "$OPTARG")
+      ;;
+  esac
+done
+
+if [[ -z "$file_path" ]] || [[ -z "$file_name" ]]; then
+  echo "File is required"
+  exit 1
+fi
+
+cat "${file_path}/${file_name}" | wc -l 
