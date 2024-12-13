7bcccf4a8d1ddeaf61e14835b1c132f5053e0071
mist <statkevich.ivan999@gmail.com>
2024-12-13 21:27:31 +0200

    added sidk_space

--- disk_space/disk_space.sh
+++ disk_space/disk_space.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+usage_in_percents=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
+
+if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to retrieve disk usage information."
+    exit 1
+fi
+
+# Перевіряємо рівень зайнятого простору
+if (( usage_in_percents >= 80 )); then
+    echo "Warning: Disk space usage is ${usage_in_percents}%, which exceeds the limit!"
+else
+    echo "All good: Disk space usage is ${usage_in_percents}%."
+fi
